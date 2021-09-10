099ed445e3dba7b63f8eb6e7334f2c1d679e637c
Michal_Piec <michalbuczek00@gmail.com>
2021-09-10 15:44:12 +0200

    Cleanup

--- DatingApp/API/Controllers/BuggyController.cs
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -16,24 +16,25 @@ namespace API.Controllers
 
         [Authorize]
         [HttpGet("auth")]
-        public ActionResult<string> GetSecret(){
+        public ActionResult<string> GetSecret()
+        {
             return "secret text";
         }
 
-        [Authorize]
+        //[Authorize]
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.Users.Find(-1);
 
-            if(thing== null)
+            if (thing == null)
             {
                 return NotFound();
             }
             return Ok(thing);
         }
 
-        [Authorize]
+        //[Authorize]
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
@@ -43,7 +44,7 @@ namespace API.Controllers
             return thingToReturn;
         }
 
-        [Authorize]
+        //[Authorize]
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
