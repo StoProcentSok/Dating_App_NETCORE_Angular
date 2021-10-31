3e8ce342d85c7996c0c33f666e699f7f30c6113b
Michal_Piec <michalbuczek00@gmail.com>
2021-10-31 12:11:59 +0100

    ORdering response result by:

--- DatingApp/API/Helpers/UserParams.cs
+++ DatingApp/API/Helpers/UserParams.cs
@@ -16,5 +16,7 @@ namespace API.Helpers
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 999;
+
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
